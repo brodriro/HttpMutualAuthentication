135180444d615da7715b0da1489eace3b1665fde
Brian Rodriguez <rzknairb@gmail.com>
2024-06-28 03:24:17 +0000

    Update to version 3

--- lib/http_mutual_authentication.dart
+++ lib/http_mutual_authentication.dart
@@ -7,86 +7,68 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class HttpAuth {
-  // ignore: non_constant_identifier_names
-  final String TAG = "HttpAuth";
+  final String _tag = "HttpAuth";
 
-  /// [authPass] Password of certificate
-  String authPass;
+  late String authPass;
+  late String pathCertificate;
+  late HttpClient client;
+  late SecurityContext context;
 
-  /// [pathCertificate] Path of client certificate. Example:'assets/certificate.p12'
-  String pathCertificate;
+  Map<String, String> _headers = {};
 
-  /// [client] HttpClient for request
-  HttpClient client;
+  HttpAuth({required this.pathCertificate, required this.authPass}) {
+    client = HttpClient();
+  }
 
-  /// [context] SecurityContext - sets [pathCertificate] and [authPass] of request
-  SecurityContext context;
-
-  /// [_headers] sets headers of request
-  Map<String, String> _headers = new Map();
-
-  /// Constructor required [pathCertificate] and [authPass]
-  HttpAuth({this.pathCertificate, this.authPass});
-
-  /// Sets all headers to request
-  setHeaders(String header, String value) {
-    _headers.addAll({header: value});
+  void setHeaders(String header, String value) {
+    _headers[header] = value;
   }
 
   HttpClientRequest _getHeaders(HttpClientRequest request) {
-    if (_headers.length > 0) {
-      _headers.forEach((key, value) => request.headers.set(key, value));
-    }
+    _headers.forEach((key, value) => request.headers.set(key, value));
     return request;
   }
 
   Future<HttpClientResponse> getRequest(String url) async {
     debugPrint("API : $url");
 
-    await this._loadContext();
+    await _loadContext();
 
     var request = await client.getUrl(Uri.parse(url));
-
     _getHeaders(request);
 
-    return await request.close();
+    return request.close();
   }
 
   Future<HttpClientResponse> postRequest(
       String url, Map<String, String> body) async {
     debugPrint("API : $url");
 
-    await this._loadContext();
+    await _loadContext();
 
     var request = await client.postUrl(Uri.parse(url));
     _getHeaders(request);
     request.add(utf8.encode(json.encode(body)));
 
-    return await request.close();
+    return request.close();
   }
 
-  /// [_loadContext] function async for build [pathCertificate], [authPass] for Mutual Authentication request.
-  Future _loadContext() async {
+  Future<void> _loadContext() async {
     try {
-      ///Convert certificate to bytes
       ByteData certificate = await rootBundle.load(this.pathCertificate);
-
-      /// set SecurityContext
       this.context = SecurityContext.defaultContext;
-
-      /// Sets certificate and password to context
       this.context.useCertificateChainBytes(certificate.buffer.asUint8List(),
           password: this.authPass);
       this.context.usePrivateKeyBytes(certificate.buffer.asUint8List(),
           password: this.authPass);
-    } catch (exception) {
-      debugPrint("$TAG: Certificate not found. $exception");
+    } catch (e) {
+      debugPrint("$_tag: Error loading certificate: $e");
+      rethrow; // Re-throw the exception to maintain proper error propagation
     }
   }
 
-  /// Convert HttpClientResponse to String
   static Future<String> parseBody(HttpClientResponse response) async {
-    return await response.transform(Utf8Decoder()).join();
+    return await utf8.decodeStream(response);
   }
 }
 
@@ -94,15 +76,11 @@ class HttpAuthBuilder {
   String authPass;
   String pathCertificate;
 
-  HttpAuthBuilder({this.authPass, this.pathCertificate});
+  HttpAuthBuilder({required this.authPass, required this.pathCertificate});
 
-  /// Generate HttpAuth
-  HttpAuth get build {
-    SecurityContext context;
-    HttpAuth httpAuth = new HttpAuth(
+  HttpAuth build() {
+    HttpAuth httpAuth = HttpAuth(
         authPass: this.authPass, pathCertificate: this.pathCertificate);
-    httpAuth.context = context;
-    httpAuth.client = new HttpClient(context: context);
     return httpAuth;
   }
 }
